2fbeac09b30d8574074232b53051dee1cdcf0de1
pvalle <paul.valle@orbian.com>
2019-01-28 13:41:00 +0000

    Initial commit

---
+++ printing.py
@@ -0,0 +1,31 @@
+print('Hello World')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+
+# using variables
+greeting = "Hello"
+name = "Paul"
+print(greeting + name)
+print(greeting + ' ' + name)
+
+#greeting = 'Hello'
+#name = input("Please enter your name: ")
+#print(greeting + ' ' + name)
+
+splitString = "\tThis string has been \nsplit over\n several \nlines"
+print(splitString)
+
+print('The pet shop owner said "No, no \'e\'s uh,...he\'s resting"')
+print("The pet shop owner said \"No, no 'e's uh,...he's resting\"")
+print('''The pet shop owner said "No, no 'e's uh,...he's resting"''')
+print("""The pet shop owner said "No, no 'e's uh,...he's resting" """)  # notice the space
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
---
+++ variables.py
@@ -0,0 +1,50 @@
+greeting = "Paul"
+_myName = "Paul"
+Paul99 = "Good"
+Paul_was_here = "Good"
+Greeting = "Hello"
+
+print(Greeting + ' ' + greeting)
+
+## below will cause an error int and String mix
+# age = 24
+# print(age)
+# print(greeting + age)
+
+a = 12
+b = 3
+c = 12.45
+
+print(a.__class__)
+print(b.__class__)
+print(c.__class__)
+print(greeting.__class__)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)    # divide returns a float
+print(a // b)   # double slash means divide but return a int
+print(a % b)
+
+print(a + b / 3 - 4 * 12)
+print((((a + b) / 3) - 4) * 12)     # correct answer = 12
+
+parrot = "Norwegian Blue"
+print(parrot[3])                    # start at zero
+print(parrot[-1])                   # start at end of string
+
+print(parrot[0:6])                  # get a range, don't include 6
+print(parrot[:6])                   # start at beginning to 6
+print(parrot[6:])                   # start at 6 until end
+print(parrot[0:6:2])                # start at 0 up to 6 skip 2
+print("1, 2, 3, 4, 5, 6, 7"[0::3])  # print just the numbers
+
+string1 = "he's "
+string2 = "probably"
+print(string1 + string2)
+print("hello " * 5)
+
+today = "Friday"
+print("day" in today)               # returns boolean
+print("parrot" in today)            # returns boolean
