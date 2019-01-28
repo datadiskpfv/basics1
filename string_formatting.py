99615a85d6a3842a8ca237470b7db3abbc7ccdc2
pvalle <paul.valle@orbian.com>
2019-01-28 13:52:36 +0000

    learning string formatting

---
+++ string_formatting.py
@@ -0,0 +1,11 @@
+age = 24
+print("My age is " + str(age) + " years")
+
+print("My age is {0} years".format(age))
+
+city = "Milton Keynes"
+print("My age is {0} years and lives in {1}".format(age, city))
+
+# Old way and no longer used depreciated - Python 2
+print("My age is %d years and lives in %s" % (age, city))
+
