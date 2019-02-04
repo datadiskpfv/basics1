a62a2fcc17c6d29b9f3921b2836824a88b9c2f0a
pvalle <paul.valle@orbian.com>
2019-02-04 13:50:41 +0000

    learning dictionaries, joins and create a mini adventure game using both

---
+++ dictionaries.py
@@ -0,0 +1,49 @@
+# dictionaries are like hash arrays, key/value pairs
+# the key is actually a hashed value
+# no duplicate keys
+# no guarantee in the order returned
+
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit",
+         "smelly fruit": "horriable smelly fruit"}
+
+# add to dictionary
+fruit["pear"] = "an odd shape apple"
+
+# overwrite element
+fruit["lime"] = "great with tequila"
+
+# delete a element
+del fruit["smelly fruit"]
+
+print(fruit)
+print(fruit["lemon"])
+print(fruit["lime"])
+
+for key in fruit:
+    print("{}: {}".format(key, fruit[key]))
+
+for key, value in fruit.items():
+    print("{}: {}".format(key, value))
+
+print("\nOrdered list: ")
+for key in sorted(list(fruit.keys())):
+    print("\t{}: {}".format(key, fruit[key]))
+
+# get tuples from dictionary, you can also go the other way
+f_tuple = tuple(fruit.items())
+print(f_tuple)
+
+print("\n---------------------------------------------------------------------")
+
+bike = {"make": "Honda", "model": "250 dream", "color": "red", "engine_size": 250}
+print(bike)
+print(bike["engine_size"])
+
+print("\n---------------------------------------------------------------------")
+
+
+
---
+++ join.py
@@ -0,0 +1,7 @@
+myList = ["a", "b", "c", "d"]
+letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "1234567890"
+
+print(", ".join(myList))
+print(", ".join(letters))
+print(", ".join(numbers))
