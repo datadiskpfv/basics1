b55b277901a808b2a814ab24a06c6d0eec4c2cda
pvalle <paul.valle@orbian.com>
2019-02-08 13:54:35 +0000

    learning to use modules

---
+++ Modules1.py
@@ -0,0 +1,11 @@
+# from turtle import *              # bad way of importing
+import turtle
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+turtle.circle(75)
+
+turtle.done()
+
+help(turtle)
