591c5167d7d28dda22106a89df63e4e6f2a91191
pvalle <paul.valle@orbian.com>
2019-02-18 08:19:44 +0000

    looking at functions

--- functions2.py
+++ functions2.py
@@ -1,22 +1,23 @@
 import tkinter
 
 
-def parabola(x):
-    y = x * x / 100
-    return y
+def parabola(para_x):
+    para_y = para_x * para_x / 100
+    return para_y
 
 
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    page.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    page.create_line(0, y_origin, 0, -y_origin, fill="black")
+    print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill="red")
+def plot(page, plot_x, plot_y):
+    page.create_line(plot_x, plot_y, plot_x + 1, plot_y + 1, fill="red")
 
 
 mainWindow = tkinter.Tk()
