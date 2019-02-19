a8d08e605720f4f444ab8018cb005cb2a7eecddb
pvalle <paul.valle@orbian.com>
2019-02-19 08:51:02 +0000

    working on black jack game

--- blackjack.py
+++ blackjack.py
@@ -32,6 +32,9 @@ def deal_card(frame):
     # pop the next card off the top of the deck
     next_card = deck.pop(0)
 
+    # add it back to the deck
+    deck.append(next_card)
+
     # add the image to a label and display the label
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
 
@@ -39,28 +42,81 @@ def deal_card(frame):
     return next_card
 
 
+def score_hand(hand):
+    # Calculate the total score of all cards in the list
+    # Only one ace can have the value 11, and this will be reduce to 1 if the hand would bust
+    score = 0
+    ace = False
+
+    for next_card in hand:
+        card_value = next_card[0]
+        if card_value == 1 and not ace:
+            ace = True
+            card_value = 11
+        score += card_value
+        # If we would bust, check if there is an ace and subtract 10
+        if score > 21 and ace:
+            score -= 10
+            ace = False
+    return score
+
+
 def deal_dealer():
-    deal_card(dealer_card_frame)
+    dealer_score = score_hand(dealer_hand)
+
+    while 0 < dealer_score < 17:
+        dealer_hand.append(deal_card(dealer_card_frame))
+        dealer_score = score_hand(dealer_hand)
+        dealer_score_label.set(dealer_score)
+
+    player_score = score_hand(player_hand)
+    if player_score > 21:
+        result_text.set("You Busted, Dealer Wins!!")
+    elif dealer_score > 21 or dealer_score < player_score:
+        result_text.set("You Win!!!!!!")
+    elif dealer_score > player_score:
+        result_text.set("Dealer Wins!!")
+    else:
+        result_text.set("Its a Draw!!!!")
 
 
 def deal_player():
-    # python creates a local variable if you change or assign a value
-    # if you dont use a global one, hence we use the global keyword
-    global player_score
-    global player_ace
-
-    card_value = deal_card(player_card_frame)[0]
-    if card_value == 1 and not player_ace:
-        player_ace = True
-        card_value = 11
-    player_score += card_value
-
-    # if we would bust, check if there is an ace and subtract
-    if player_score > 21 and player_ace:
-        player_score -= 10
+    player_hand.append(deal_card(player_card_frame))
+    player_score = score_hand(player_hand)
     player_score_label.set(player_score)
     if player_score > 21:
-        result_text.set("Dealer Wins!")
+        result_text.set("You Busted, Dealer Wins!!")
+
+
+def new_game():
+    global dealer_card_frame
+    global player_card_frame
+    global dealer_hand
+    global player_hand
+
+    # embedded frame to hold the card images
+    dealer_card_frame.destroy()
+    dealer_card_frame = tkinter.Frame(card_frame, background="green")
+    dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=1)
+    # embedded frame to hold the card images
+    player_card_frame.destroy()
+    player_card_frame = tkinter.Frame(card_frame, background="green")
+    player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+
+    result_text.set("")
+
+    # recreate the lists
+    dealer_hand = []
+    player_hand = []
+
+    deal_player()
+    dealer_hand.append(deal_card(dealer_card_frame))
+    dealer_score_label.set(score_hand(dealer_hand))
+    deal_player()
+
+
+def shuffle():
+    random.shuffle(deck)
 
 
 ###########################
@@ -89,8 +145,6 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
 
 player_score_label = tkinter.IntVar()
-player_score = 0
-player_ace = False
 
 tkinter.Label(card_frame, text="Player", background="green", fg='white').grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg='white').grid(row=3, column=0)
@@ -109,6 +163,12 @@ dealer_button.grid(row=0, column=0)
 player_button = tkinter.Button(button_frame, text="Player", width=10, command=deal_player)
 player_button.grid(row=0, column=1)
 
+new_game_button = tkinter.Button(button_frame, text="New Game", width=10, command=new_game)
+new_game_button.grid(row=0, column=2)
+
+shuffle_button = tkinter.Button(button_frame, text="Shuffle", width=10, command=shuffle)
+shuffle_button.grid(row=0, column=3)
+
 # load cards
 cards = []
 load_images(cards)
@@ -116,10 +176,12 @@ print(cards)
 
 # create a new deck of cards and shuffle them
 deck = list(cards)
-random.shuffle(deck)
+shuffle()
 
 # create the lists to store the dealer and player hands
 dealer_hand = []
 player_hand = []
+
+new_game()
 
 mainWindow.mainloop()
